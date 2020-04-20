7bd5508e8841ed0396b0dade03b2812de3e4f36d
Travis Watson <watsonjamest@gmail.com>
2020-04-20 14:50:50 -0500

    Add postgres db

---
+++ Data/DogMedicationTrackerContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogMedicationTracker.Data
+{
+    public class DogMedicationTrackerContext : DbContext
+    {
+        public DogMedicationTrackerContext(DbContextOptions<DogMedicationTrackerContext> options) :base(options)
+        {
+                
+        }
+
+
+    }
+}
