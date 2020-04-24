cb8365c0bcaefce21487cd687294e3a65dd788b9
James <email@email.com>
2020-04-24 15:46:03 -0500

    Add dogs controller and index view

---
+++ Controllers/DogsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DogMedicationTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogMedicationTracker.Controllers
+{
+    public class DogsController : Controller
+    {
+
+        private readonly DogMedicationTrackerContext context;
+
+        public DogsController(DogMedicationTrackerContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
